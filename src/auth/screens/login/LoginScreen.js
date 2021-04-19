2047e4071ba92eae14a75ac18d525e402a2782b2
marvaibanc <marvaicolombia@gmail.com>
2021-04-19 13:13:24 -0500

    Yohan, se agregan screens

---
+++ src/auth/screens/login/LoginScreen.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import {
+  SafeAreaView,
+  //  ScrollView,
+   StatusBar,
+  //  StyleSheet,
+   Text,
+  //  useColorScheme,
+   View,
+   Image,
+   TextInput,
+   Button,
+   Alert,
+   ImageBackground,
+  
+ } from 'react-native';
+ 
+ 
+//  import {
+//    Colors,
+//    DebugInstructions,
+//    Header,
+//    LearnMoreLinks,
+//    ReloadInstructions,
+//  } from 'react-native/Libraries/NewAppScreen';
+ import Styles from './LoginStyles';
+ const LoginScreen= () => {
+   
+   return (
+     <SafeAreaView style={Styles.container}>
+       <StatusBar backgroundColor="orange"></StatusBar>
+     <ImageBackground style={{flex: 1}} source={require('./img/login-back.jpg')}>
+       <View style={Styles.iconContainer}>
+              <Image style={Styles.logo} source={require('./img/logo.png')}></Image>
+       </View>
+       <View style={Styles.inputContainer}>
+            <View style={Styles.searchSection}>
+              
+              <TextInput
+              style={Styles.input}
+              placeholder="Usuario"
+              underlineColorAndroid="transparent"
+              />
+            </View>
+           <Button
+           onPress={() => Alert.alert('Debe llenar los campos')}
+           title="Ingresar"
+           color="#F3671C"
+           accessibilityLabel="Calmado muelon"
+           style={Styles.btn}/>
+       </View>
+     </ImageBackground>
+     </SafeAreaView>
+   );
+ };
+ 
+ 
+ 
+ export default LoginScreen;
+ 
---
+++ src/auth/screens/login/LoginStyles.js
@@ -0,0 +1,78 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    iconContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        flex: 2,
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    logo: {
+      width: 100,
+      height: 100,
+      marginTop: 14
+    },
+    logoContainer: {
+      flex: 2,
+      alignItems: "center"
+    },
+    textContainer:{
+        flex: 1,
+        alignItems: "flex-end",
+        paddingRight: 15,
+        justifyContent: "flex-end",
+        paddingBottom: 15
+    },
+    textLogo: {
+        color: "white",
+        fontStyle: "italic",
+        fontSize: 22
+    },
+    // input: {
+    //   height:40,
+    //   backgroundColor: "white",
+    //   width: Dimensions.get('screen').width * 0.9,
+    //   paddingHorizontal: 22,
+    //   borderRadius: 16,
+    //   marginTop: 40,
+    //   shadowColor: "#000",
+    //   shadowOpacity: 0.15,
+    //   shadowRadius: 2.84,
+    //   elevation: 4,
+    // },
+    btn: {
+        width: Dimensions.get('screen').width * 0.9,
+    },
+    searchSection: {
+      height:40,
+      width: Dimensions.get('screen').width * 0.9,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      borderRadius: 16,
+      alignItems: 'center',
+      backgroundColor: '#fff',
+    },
+    searchIcon: {
+        padding: 10,
+    },
+    input: {
+        flex: 1,
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
+        borderRadius: 16,
+        backgroundColor: '#fff',
+        color: '#424242',
+    },
+  });
+
+export default Styles;
+
