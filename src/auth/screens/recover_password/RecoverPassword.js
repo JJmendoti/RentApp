44b29c6e43423a22466b924a34cfa1b5236bb0fc
Jhon Mendoza <jmendoti.cesde@gmail.com>
2021-04-20 08:16:20 -0500

    Jhon, Se empieza a crear vista de RecoverPassword

---
+++ src/auth/screens/recover_password/RecoverPassword.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  //  ScrollView,
+  StatusBar,
+  //  StyleSheet,
+  Text,
+  //  useColorScheme,
+  View,
+  Image,
+  TextInput,
+  Button,
+  Alert,
+  ImageBackground,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
+import Styles from './RecoverStyles';
+const RecoverPassword = ({navigation}) =>{
+    const[email,setEmail] = useState("")
+        const validateRecoverPass = () =>{
+        if(email === ""){
+          Alert.alert("El campo Email esta vacio")
+          
+        }else {
+         setEmail("");
+        }
+       
+      }
+      return (
+          <ScrollView style={}>
+              
+          </ScrollView>
+      )
+
+
+};
+
+export default RecoverPassword;
